36cf073c7b4799e1a079be90233940c0535d859e
Jeet Kundu <kundujeet916@gmail.com>
2026-05-08 22:21:54 +0530

    Updated code

--- covid.py
+++ covid.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# ================= COVID DATA =================
 
 data = {
     "Country": ["India", "USA", "Brazil", "Russia", "Japan"],
@@ -10,20 +9,17 @@ data = {
     "Deaths": [530000, 1120000, 700000, 390000, 74000]
 }
 
-# ================= CREATE DATAFRAME =================
 
 df = pd.DataFrame(data)
 
-# Index Start From 1
+
 df.index = range(1, len(df) + 1)
 
-# ================= SHOW DATA =================
 
 print("\n========== COVID-19 DATA ANALYSIS ==========\n")
 
 print(df)
 
-# ================= TOTAL CASES =================
 
 total_cases = df["Cases"].sum()
 total_deaths = df["Deaths"].sum()
@@ -31,14 +27,12 @@ total_deaths = df["Deaths"].sum()
 print("\nTotal Cases =", total_cases)
 print("Total Deaths =", total_deaths)
 
-# ================= HIGHEST CASE COUNTRY =================
 
 highest = df.loc[df["Cases"].idxmax()]
 
 print("\nCountry With Highest Cases:")
 print(highest["Country"])
 
-# ================= BAR CHART =================
 
 plt.figure(figsize=(8,5))
 
